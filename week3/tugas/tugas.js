1c939cb234fc35934ffd06535a7143418ade9b80
ruqfarawayy <lupinibrahim@gmail.com>
2022-10-17 23:46:06 +0700

    nugas buat bootcamp

--- week3/tugas/tugas.js
+++ week3/tugas/tugas.js
@@ -1,10 +1,37 @@
+const bilangan = [2, 7, 15, 24, 19, 9, 5, 34];
+
 // input : angka array positif
 // output : Variabel berisi penjumlahan seluruh elemen array
+function totalArr (num) {
+    let total = 0;
+    for (let i = 0; i < num.length; i++ ) {
+        total += num[i];
+    }
+    return total;
+}
+
+// console.log(totalArr(bilangan));
+
 
 //input : array angka positif
 // output : array angka positif berurutan dari kecil ke besar
+function urutan (arr) {
+    arr.sort(function (a, b) {return a - b});
+   console.log(arr)
+}
+
+// urutan(bilangan);
 
 // input : array angka positif
 // output : array angka prima
+function bilanganPrima (arr) {
+    temp = [];
+    for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 == 1 || arr[i] == 2){
+        temp.push(arr[i]);
+        }
+    }
+    return urutan (temp);
+};
 
-// catatan : dengan gaya fungsional
+console.log(bilanganPrima(bilangan));
