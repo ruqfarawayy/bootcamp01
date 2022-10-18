381eb81c4a6d7295233c128ecd4fd23a65c62d71
ruqfarawayy <lupinibrahim@gmail.com>
2022-10-18 14:30:13 +0700

    menyelesaikan challenge

--- week3/tugas/maparray.js
+++ week3/tugas/maparray.js
@@ -1,6 +1,12 @@
-const bilangan = [3, 5, 7, 8, 2, 4]
+const bilangan = [1, 2, 13, 25, 17, 8, 4]
 
-// const pangkat = bilangan.map(i => i**2);
+// const urut = bilangan.sort((a,b) => (a-b));
+
+// console.log(urut);
+
+const pangkat = bilangan.map(i => {
+    return i**2
+});
 
 // menggunakan function anonymous
 // const pangkat = bilangan.map(function (a) {
@@ -8,9 +14,9 @@ const bilangan = [3, 5, 7, 8, 2, 4]
 //     return a**2 + c;
 // })
 
-const pangkat = bilangan.map( (a) => {
-    let c = 2;
-    return a**2 + c;
-})
+// const pangkat = bilangan.map((a) => {
+//     let c = 2;
+//     return a**2 + c;
+// })
 
 console.log(pangkat);
