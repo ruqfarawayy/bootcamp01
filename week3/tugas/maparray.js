b8eabf63b73415b25e9409fb5cecfa2a7901fdda
ruqfarawayy <lupinibrahim@gmail.com>
2022-10-18 11:12:17 +0700

    mencoba menggunakan function

--- week3/tugas/maparray.js
+++ week3/tugas/maparray.js
@@ -1,5 +1,8 @@
 const bilangan = [3, 5, 7, 8, 2, 4]
 
-const pangkat = bilangan.map(i => i**2);
+// const pangkat = bilangan.map(i => i**2);
 
+const pangkat = bilangan.map(function (i){
+    return i**2;
+})
 console.log(pangkat);
