c81e004593488b78f2097684b4a46618cb390ddc
ruqfarawayy <lupinibrahim@gmail.com>
2022-10-19 11:32:40 +0700

    ini nyoba try catch error

---
+++ week3/errorhandling.js
@@ -0,0 +1,12 @@
+function getErrorInObject() {
+    try {
+        nonExistentFunction();
+    } catch (errorVariable){
+        return {
+            nama:errorVariable.name,
+            pesan:errorVariable.message,
+            susunan:errorVariable.stack
+            }
+        }
+    }   
+
--- week3/trycatch.js
+++ week3/trycatch.js
@@ -1,5 +1,17 @@
+// try {
+//     nonExistentFunction();
+// } catch (errorVariable) {
+//     console.log(errorVariable);
+// }
+
+// Example using
+
+let arr = [1, 2, 3, 4, 5];
+
 try {
-    nonExistentFunction();
-} catch (errorVariable) {
-    console.log(errorVariable);
+    console.log(getElement(arr, 4));
+} 
+catch (e) {
+    console.log(e.message);
 }
+console.log("The program continued executing!");
