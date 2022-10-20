e4aacdaae0f883c3a9a92d5fe6820c67a3af436e
ruqfarawayy <lupinibrahim@gmail.com>
2022-10-20 15:14:21 +0700

    udah paling update

--- week3/CRUD-2/js/add.js
+++ week3/CRUD-2/js/add.js
@@ -1,24 +1,19 @@
-import * as data from "./data";
-import * as tampil from "./tampil";
 
-let btnSimpan = document.getElementById('btn-simpan');
-btnSimpan.addEventListener("click", simpan)
 
-function simpan () {
+function simpan (profiles) {
     let nikInput = document.getElementById('tambahNik');
     let namaInput = document.getElementById('tambahNama');
     let ageInput = document.getElementById('tambahUsia');
 
-    data.profiles.push({
+   profiles.push({
         nik:nikInput.value,
         nama:namaInput.value,
         age: parseInt(ageInput.value),
     });
 
-    tampil.tampilData(data.profiles);
     nikInput.value = '';
     namaInput.value = '';
     ageInput.value = '';
 };
     
-export {btnSimpan};
+export {simpan};
--- week3/CRUD-2/script.js
+++ week3/CRUD-2/script.js
@@ -1,11 +1,57 @@
 import * as tambah from "./js/add.js";
 import * as data from "./js/data.js";
-import * as read from "./js/tampil.js";
+
+// console.log(data.profiles);
 
 // JSON.parse(localStorage.getItem('profiles'));
+let btnSimpan = document.getElementById('btn-simpan');
+btnSimpan.addEventListener("click", () => {
+    tambah.simpan(data.profiles);
+    tampilData(data.profiles);
+} );
 
-tambah.btnSimpan;
 
-read.tampilData(data.profiles);
+// btnDelete.addEventListener("click")
+// btnDelete.addEventListener("click", () => {
+//     console.log("hapus yaaaa");
+// } );
 
+function tampilData(profileData) {
+    let tBody = document.getElementById('printData');  
+    tBody.innerHTML = "";
+    // console.log(tBody);
+    for (let i in profileData) {
+        let nik = profileData[i].nik;
+        let nama = profileData[i].nama;
+        let age = profileData[i].age;
+        let tRow = `<tr>
+                    <th>${parseInt(i) + 1}</th>
+                    <td>${nik}</td>
+                    <td>${nama}</td>
+                    <td>${age}</td>
+                    <td>
+                        <button class="btn btn-primary" type="button" data-bs-toggle="modal" data-bs-target="#ubahData" onclick="selectData(${i})">Ubah</button>
+                        <button class="btn btn-primary btnDel" type="button" data-id="${i}">Hapus</button>
+                    </td>
+                </tr>`;
+        tBody.innerHTML += tRow;
+    };
+};
+
+
+tampilData(data.profiles);
+
+// let btnDelete = document.querySelectorAll('.btnDel');
+// btnDelete.addEventListener("click", (e) => {
+//     console.log("heheehe", e)
+// });
+
+
+let btnDelete = Array.from(document.getElementsByClassName('btnDel'));
+btnDelete.forEach(del => {
+    del.addEventListener("click", (e)=> {
+        let id = e.getAttribute("data-id");
+        console.log(id);
+    });
+});
 // localStorage.setItem('profiles', JSON.stringify([{nik: 2141312389, nama: "Ganda", age: 34}]));
